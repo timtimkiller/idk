70cc8cb242afe44ac96d64b85eec3363496d050f
timtimkiller <97465794+timtimkiller@users.noreply.github.com>
2022-06-22 15:46:08 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,12 @@
 let Score = 0
+let index = 0
 input.onPinPressed(TouchPin.P0, function () {
 	
 })
 input.onButtonPressed(Button.A, function () {
     (music.playMelody("A E D E G D B F ", 120),SoundExpressionPlayMode.UntilDone)
 Score += -1
+    index += 1
 })
 input.onButtonPressed(Button.B, function () {
     Score += 1
@@ -18,4 +20,10 @@ Score = 0
     if (Score == -1) {
         Score = 0
     }
-})
+    if (index == 3) {
+        basic.showString("ERROR! ERROR! ERROR!")
+        music.playMelody("C5 C E C A A E C5 ", 120)
+        music.playMelody("G F A C C5 A A A ", 120)
+        music.playMelody("G A A A A A A A ", 120)
+        music.playMelody("A A A A A A A A ", 120)
+    } stopmelody()
